e880ddb6682731635a8885a712728aaa76a4e465
DavidFarm <david.farm@amunholding.se>
2025-11-24 20:50:49 +0100

    sql syntax to please sqlfluff

--- models/marts/sales/fct_sales_pareto.sql
+++ models/marts/sales/fct_sales_pareto.sql
@@ -1,18 +1,20 @@
 -- models/marts/sales/fct_sales_pareto.sql
 with tot_sales as (
-    select * from {{ref("tot_sales_fact")}}
+    select * from {{ref("tot_sales_fact")}} 
 ),
 products as (
-    select * from {{ref("products_dim_product")}}
+    select * from {{ref("products_dim_product")}} 
 ),
 category as (
-    select * from {{ref("products_dim_subcategory")}}
+    select * from {{ref("products_dim_subcategory")}} 
 ),
 total_all as (
 	select SUM(order_quantity * sales_amount) as total from tot_sales
 ),
 total_prod as (
-	select product_key, SUM(order_quantity * sales_amount) as sales
+	select 
+        product_key, 
+        sum(order_quantity * sales_amount) as sales
 	from tot_sales
 	group by product_key
 ),
@@ -21,10 +23,10 @@ final AS (
 		s.product_key,
 		s.sales as total_sales,
 		s.sales / total_all.total * 100 as perc_total,
-		SUM(s.sales) OVER (ORDER BY s.sales DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as running_total,
-        RANK() OVER(ORDER BY s.sales DESC) as ranking
+		sum(s.sales) over (order by s.sales desc rows between unbounded preceding and current row) as running_total,
+        rank() over(order by s.sales desc) as ranking
 	from
-		total_prod s, total_all
+		total_prod s cross join total_all
 	)
 select 
 	p.product_name,
@@ -35,5 +37,5 @@ select
     s.running_total / total_all.total as running_perc,
     s.ranking
 from products p join final s on s.product_key = p.product_key 
-	join category c on c.subcategory_key = p.subcategory_key, total_all
+	join category c on c.subcategory_key = p.subcategory_key cross join total_all
 where running_total <= total_all.total * 0.8
